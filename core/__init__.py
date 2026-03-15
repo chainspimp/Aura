2a3eb39a8e7be9f6fc689de005ea697bb0db95f9
chainspimp <chains.pimp@gmail.com>
2026-03-15 13:35:51 -0500

    Update __init__.py

--- core/__init__.py
+++ core/__init__.py
@@ -3,7 +3,6 @@ Core functionality for AURA AI assistant
 """
 
 from core.audio import InterruptibleTTS, text_to_speech, play_audio
-from core.speech import listen
 from core.memory import load_memory, save_memory, add_memory, get_context
 from core.utils import get_time, get_time_str, get_relative_time, get_time_context
 
